dde45d5af3e6b9672cc37553a9824760ec495646
Marius Horga <mhorga@gmail.com>
2015-01-08 17:26:53 +0200

    Created a category on NSArray

---
+++ Example/NSArray+Randomize.h
@@ -0,0 +1,15 @@
+//
+//  NSArray+Randomize.h
+//  randomizeCategoryOnNSArray
+//
+//  Created by Marius Horga on 1/8/15.
+//  Copyright (c) 2015 Marius Horga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Randomize)
+
+- (NSArray *)randomizeArray;
+
+@end
